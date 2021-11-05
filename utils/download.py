4b991e42406dc7d4aab2adf7968903d045cb1275
Mahad Janjua <mahad.j15@hotmail.com>
2021-11-05 14:55:35 -0400

    Handle folder creation for holding the data

--- utils/download.py
+++ utils/download.py
@@ -1,9 +1,14 @@
 import gdown
 import zipfile
+import os
 
 LINK_TO_ZIP = 'https://drive.google.com/file/d/1lnniHRHTVvQEbiKjqILO0xZG0kq4T_zl/view?usp=sharing'
 DATA_ZIPPED = 'utils/data/data.zip'
 DATA_UNZIPPED = 'utils/data'
+
+# Create data folder
+def create_folder():
+    os.makedirs(DATA_UNZIPPED)
 
 # Function to download and unzip data
 def download():
@@ -14,5 +19,6 @@ def unzip():
         zip_ref.extractall(DATA_UNZIPPED)
 
 if __name__ == "__main__":
+    create_folder()
     download()
     unzip()
