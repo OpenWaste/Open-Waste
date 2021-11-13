53d83d3a8d3dbd2488f58ae5fb3fe555895a8c8d
pirasunt <piravien.sunt@hotmail.com>
2021-11-12 21:45:17 -0500

    chore: add secret key to environment variable #54
    - Also added a .env.example to be used by people who clone it for the first time

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i963=)@ets@!8_!ks31b87q1l3axw(j8ai6j*njt6mb2col%f='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
