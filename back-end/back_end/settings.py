0927276c9ebae12d37bc6ad08ae681da31269091
Daniel Gauvin <danielgauvin01@gmail.com>
2021-11-21 02:31:42 -0500

    feat: all database models added except images
    - models are founds in backend>components>models.py

--- back-end/back_end/settings.py
+++ back-end/back_end/settings.py
@@ -18,6 +18,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
