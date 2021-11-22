93f422f1d92ad4ab38822524fc3d4397f2f43f11
Daniel Gauvin <danielgauvin01@gmail.com>
2021-11-22 14:22:17 -0500

    bug: media file no longer duplicating
    - when creating model object, media folder will no longer be duplicated

--- back-end/back_end/settings.py
+++ back-end/back_end/settings.py
@@ -18,8 +18,9 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+#this is where all photos are located for the database to fetch
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
