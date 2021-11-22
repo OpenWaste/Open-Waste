21a6ccc54c8803e38c73f2216cf7306ef6902741
Daniel Gauvin <danielgauvin01@gmail.com>
2021-11-22 14:02:55 -0500

    feat: added Image field for models #74
    - implemented models in backend>Components>models.py
    -registered them on the admin panel to easily interact
    -created a super user with user: admin and password: admin

--- back-end/Components/admin.py
+++ back-end/Components/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+# admin page can interact with these models
+#admin.site.register(Category)
+admin.site.register(DWUser)
+admin.site.register(Building)
+admin.site.register(Building_images)
+admin.site.register(Bin)
+admin.site.register(Bin_images)
