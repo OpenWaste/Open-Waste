61bacf74567dc2da0e659290ca95411f5871e6a4
Daniel Gauvin <danielgauvin01@gmail.com>
2021-11-26 15:10:36 -0500

    feature: added trash_accepted table
    - added a table to complete er_diagram
    - implemented @pirasunt requested changes
    - reverted deletion of env.example

--- back-end/Components/models.py
+++ back-end/Components/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.conf import settings
 # Create your models here.
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    sample_image = models.ImageField(null=True, blank=True, upload_to='sample_images/')
+    category_sample_image = models.ImageField(null=True, blank=True, upload_to=settings.CATEGORY_IMG_PATH)
 
 
 class DWUser(models.Model):
@@ -21,27 +21,35 @@ class Building(models.Model):
 
 class Building_images(models.Model):
     buildling = models.ForeignKey(Building, on_delete=models.CASCADE, default=1)
-    building_image = models.ImageField(null=True, blank=True, upload_to='building_images/')
+    building_image = models.ImageField(null=True, blank=True, upload_to=settings.BUILDING_IMG_PATH)
 
 
 class Bin(models.Model):
-    categories_choices = (
-        ('Category1', 'Category1'),
-        ('Category2', 'Category2'),
-        ('Category3', 'Category3'),
-    )
-
+    # CATEGORIES WILL NEED TO BE CHANGED WITH MORE INFO
+    CATEGORY1 = 'C1'
+    CATEGORY2 = 'C2'
+    CATEGORY3 = 'C3'
+    WASTE_CATEGORY_CHOICES = [
+        (CATEGORY1, 'Category1'),
+        (CATEGORY2, 'Category2'),
+        (CATEGORY3, 'Category3'),
+    ]
+    
     building_id = models.ForeignKey(Building, on_delete=models.CASCADE, default=1)
     address = models.CharField(max_length=200)
     latitude = models.DecimalField(max_digits=6, decimal_places=6)
     longitude = models.DecimalField(max_digits=6, decimal_places=6)
     floor_num = models.IntegerField()
     location_description = models.TextField()
-    accepted_categories = models.CharField(max_length=30, blank=True, null=True, choices=categories_choices)
+    accepted_categories = models.CharField(max_length=30, blank=True, null=True, choices=WASTE_CATEGORY_CHOICES)
 
 
 class Bin_images(models.Model):
     bin_id = models.ForeignKey(Bin, on_delete=models.CASCADE, default=1)
-    bin_images = models.ImageField(null=True, blank=True, upload_to='bin_images/')
+    bin_images = models.ImageField(null=True, blank=True, upload_to=settings.BIN_IMG_PATH)
+
+
+class Trash_Accepted(models.Model):
+    accepted_image = models.ImageField(null=True, blank=True, upload_to=settings.ACCEPTED_TRASH_IMG_PATH)
 
 
