de833474ebfda90f887e6a540b5fbe118ec5da0d
Mahad Janjua <mahad.j15@hotmail.com>
2021-12-06 00:00:25 -0500

    fix: ensure model is saved where it can be used by predictor in back-end #129

--- back-end/Components/machine_learning/predictor.py
+++ back-end/Components/machine_learning/predictor.py
@@ -19,14 +19,12 @@ class Predictor:
         # Load model as it is in the model.pt file
         self.__model = models.resnet18(pretrained=True)
         num_ftrs = self.__model.fc.in_features
-        for param in self.__model.parameters():
-            param.requires_grad = False
         self.__model.fc = nn.Sequential(
             nn.Linear(num_ftrs, 256),
             nn.ReLU(),
             nn.Linear(256, len(Predictor.__CLASS_NAMES))
         )
-        self.__model.load_state_dict(torch.load(Predictor.__ML_MODEL_PATH, map_location=torch.device('cpu')), strict=False)
+        self.__model.load_state_dict(torch.load(Predictor.__ML_MODEL_PATH, map_location=torch.device('cpu')))
         self.__model.eval()  # Set in evaluation mode (not training)
 
     def evaluate_image(self, image_path: str) -> str:
@@ -36,6 +34,6 @@ class Predictor:
 
         # Predict
         prediction = self.__model(image[None, ...])  # pass image to model
-        prediction = prediction.max(1, keepdim=True)[
-            1]  # get max value in the first dimension of the tensor, preserve dimensions
+        # get max value in the first dimension of the tensor, preserve dimensions
+        prediction = prediction.max(1, keepdim=True)[1]
         return Predictor.__CLASS_NAMES[prediction.item()]  # grab index and use to take classname
