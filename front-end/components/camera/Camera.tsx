0d58718889a1f7e193d51995f70a8f08f0a49c60
pirasunt <piravien.sunt@hotmail.com>
2021-12-12 18:57:45 -0500

    task: add code to take picture and upload to cloud #90

--- front-end/components/camera/Camera.tsx
+++ front-end/components/camera/Camera.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {Text, View} from 'react-native';
 import { Camera } from "expo-camera";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import style from "../../styles/camera_style";
-import { Button, NativeBaseProvider, useTheme } from 'native-base';
+import { Button, NativeBaseProvider} from 'native-base';
+import Service from "../../service/service";
+import { manipulateAsync, SaveFormat } from 'expo-image-manipulator';
 
 export default function displayCamera() { 
   const [hasPermission, setHasPermission] = useState(null);
@@ -22,17 +24,25 @@ export default function displayCamera() {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
+  let camera:Camera;
   return (
     <NativeBaseProvider>
       <View style={style.container}>
-        <Camera ratio={"16:9"} style={style.camera} type={type}>
+        <Camera ratio={"16:9"} style={style.camera} type={type} ref={r => camera = r}>
           <View style={style.footer}>
-            <MaterialIcons name="file-upload" size={60} color="#FFFFFF" />
+            <MaterialIcons onPress={() => {
+              camera.takePictureAsync().then(o => {
+                manipulateAsync(o.base64, [], {format:SaveFormat.JPEG}).then(convertedImage => {
+                  Service.submitImagePrediction(convertedImage.base64?.split(",")[1])
+  
+                })
+              })
+            }} name="file-upload" size={60} color="#FFFFFF" />
             <MaterialIcons name="cancel" size={60} color="#EA4335" />
             <Button color="#FFFFFF" height={60}>Next</Button>
           </View>
         </Camera>
       </View>
     </NativeBaseProvider>
-  );
-}
+    );
+  }
