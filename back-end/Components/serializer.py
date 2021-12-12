52b4b9d9420aa95abf57c47e9d05e534ae481a87
pirasunt <piravien.sunt@hotmail.com>
2021-12-12 18:49:52 -0500

    task: update backend to accept base64 string instead of form image #111

--- back-end/Components/serializer.py
+++ back-end/Components/serializer.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 
 class ImageRecognitionSerializer(serializers.Serializer):
-    image = serializers.ImageField(allow_empty_file=False)
+    image = serializers.CharField()
