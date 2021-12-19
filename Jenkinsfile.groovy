1c0168058df81b386eb00649e8e7941c623dd1dd
Michael Lee <mleelwh@gmail.com>
2021-12-19 14:56:44 -0500

    chore: remove git checkout from jenkinsfikle #119
    jenkins multi-branch pipeline automatically checks out branch. Remove the unnecessary scripted checkout.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,35 +4,24 @@ pipeline {
   tools {nodejs "node"}
     
   stages {
-        
-    stage('Git') {
-      steps {
-        //Check out the triggering branch using the personal access token
-        git branch: "${env.BRANCH_NAME}",
-        credentialsId: 'de474f75-b8e7-480c-a217-2cd52ef98b48',
-        url: 'https://github.com/mlee97/SOEN-490-Attack-On-SOEN.git'
-      }
-    }  
     
     stage('SonarQube analysis') {
-        environment {
-            SCANNER_HOME = tool 'Sonar-scanner'
+      environment {
+          SCANNER_HOME = tool 'Sonar-scanner'
+      }
+      steps {
+      //Perform SonarQube Analysis on front-end and back-end directories
+      withSonarQubeEnv(credentialsId: 'd56e9146-e2fa-4ee5-b50c-4dfad9c7abb8', installationName: 'Sonar') {
+        sh '''$SCANNER_HOME/bin/sonar-scanner \
+        -Dsonar.projectKey=projectKey \
+        -Dsonar.projectName=projectName \
+        -Dsonar.sources=./front-end,./back-end \
+        -Dsonar.java.binaries=target/classes/ \
+        -Dsonar.exclusions=src/test/java/****/*.java \
+        -Dsonar.java.libraries=/var/lib/jenkins/.m2/**/*.jar \
+        -Dsonar.projectVersion=${BUILD_NUMBER}-${GIT_COMMIT_SHORT}'''
         }
-        steps {
-        //Perform SonarQube Analysis on front-end and back-end directories
-        withSonarQubeEnv(credentialsId: 'd56e9146-e2fa-4ee5-b50c-4dfad9c7abb8', installationName: 'Sonar') {
-            sh '''$SCANNER_HOME/bin/sonar-scanner \
-            -Dsonar.projectKey=projectKey \
-            -Dsonar.projectName=projectName \
-            -Dsonar.sources=./front-end,./back-end \
-            -Dsonar.java.binaries=target/classes/ \
-            -Dsonar.exclusions=src/test/java/****/*.java \
-            -Dsonar.java.libraries=/var/lib/jenkins/.m2/**/*.jar \
-            -Dsonar.projectVersion=${BUILD_NUMBER}-${GIT_COMMIT_SHORT}'''
-            }
-        }
-    }
-        
+      }
+    }    
   }
-
 }
