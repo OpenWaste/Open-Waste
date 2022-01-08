59c162280fb5fc6ce6b0ed27f69516e642cb13db
CeliaCaii <47066791+CeliaCaii@users.noreply.github.com>
2022-01-07 23:03:30 -0500

    task: create guest profile ui #150

--- front-end/components/profile/Profile.tsx
+++ front-end/components/profile/Profile.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableHighlight } from "react-native";
+import style from "../../styles/profile-style";
+import { Button, NativeBaseProvider } from 'native-base';
 
 export class Profile extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Profile</Text>
-      </View>
+      <NativeBaseProvider>
+        <View>
+          <View style={style.header}></View> 
+          
+          {/* TO DO: Pull profile pic from database. */}
+          <Image style={style.profilePic} source={{uri: 'https://www.gravatar.com/avatar/d41d8cd98f00b204e9800998ecf8427e?size=192&d=mm'}} />
+          
+          <Text style={style.username}> Guest </Text>
+          <Button style={style.loginBtn}> Log In </Button>
+        </View>
+      </NativeBaseProvider>
     );
   }
 }
