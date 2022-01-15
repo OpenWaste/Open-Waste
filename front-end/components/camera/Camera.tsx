6e9a87495b836f9faf98f237b2692965c671d714
pirasunt <piravien.sunt@hotmail.com>
2022-01-15 17:41:23 -0500

    feat: completed image-recognition front-end #5

--- front-end/components/camera/Camera.tsx
+++ front-end/components/camera/Camera.tsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
-import {Alert, Text, View} from 'react-native';
+import { Text, View, Modal, TouchableHighlight, Image, ActivityIndicator } from 'react-native';
 import { Camera } from "expo-camera";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import style from "../../styles/camera-style";
-import { Button, NativeBaseProvider} from 'native-base';
+import { Button, NativeBaseProvider, Flex, Spacer  } from 'native-base';
 import Service from "../../service/service";
-import { manipulateAsync, SaveFormat } from 'expo-image-manipulator';
 import { useIsFocused } from '@react-navigation/native';
-import { color } from "native-base/lib/typescript/theme/styled-system";
+import MapView from 'react-native-maps';
 
-export default function displayCamera() { 
+export default function displayCamera() {
   const isFocused = useIsFocused();
   const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalText, setModalText] = useState("")
+  const [picTaken, setPicTaken] = useState(false)
+  const [picURI, setPicURI] = useState("")
 
   useEffect(() => {
     (async () => {
@@ -21,33 +23,94 @@ export default function displayCamera() {
     })();
   }, []);
 
+
   if (hasPermission === null) {
     return <View />;
   }
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-  let camera:Camera;
-  if(isFocused){
+  let camera: Camera;
+  if (isFocused) {
     return (
+      
       <NativeBaseProvider>
-        <View style={style.container}>
-          <Camera ratio={"16:9"} style={style.camera} type={type} ref={r => camera = r}/>
+        <View style={style.fullScreenView}>
+          {picTaken ? <Image style={style.fullScreenView} source={{ uri: picURI }} /> : <Camera ratio={"16:9"} style={style.fullScreenView} type={Camera.Constants.Type.back} ref={r => camera = r} />}
+          {picTaken ?
+          <View style={style.predictionTextContainer}>
+            { modalText.length > 0 ? <Text style={style.predictionText}>{modalText}</Text>: <ActivityIndicator size={100} color="#95E0D3"/>}
+          </View> : <></>
+          }
+
           <View style={style.footer}>
-              <MaterialIcons onPress={() => {
-                camera.takePictureAsync().then(o => {
-                  manipulateAsync(o.base64, [], {format:SaveFormat.JPEG}).then(convertedImage => {
-                    Service.submitImagePrediction(convertedImage.base64?.split(",")[1])
+          <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        setModalVisible(false);
+      }}>
+      <View style={style.centeredView}>
+        <View style={style.modalView}>
+          
+          <MaterialIcons style={style.modalCloseButton} name="cancel"  size={30} onPress={() => setModalVisible(false)}/>
+          <MapView style={style.map}
+              initialRegion={{
+                latitude: 45.494862,
+                longitude: -73.57790,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+              }} />
+        </View>
+      </View>
+    </Modal>
+
+            
+
+            
+            <TouchableHighlight activeOpacity={0.6} underlayColor={ 'transparent' }>
+              <MaterialIcons name="file-upload" size={60} color="#FFFFFF" />
+              </TouchableHighlight>
+
+            {
+              !picTaken ?
+              <TouchableHighlight activeOpacity={0.6} underlayColor={ 'transparent' } onPress={() => {
+                camera.takePictureAsync({ base64: true}).then(picture => {
+                  setPicURI(picture.uri)
+                  setPicTaken(true)
+                  Service.submitImagePrediction(picture.base64).then(response => {
+                    setModalText(response.prediction)
                   })
                 })
-              }} name="file-upload" size={60} color="#FFFFFF" />
-              <MaterialIcons name="cancel" size={60} color="#EA4335" />
-              <Button color="#FFFFFF" height={60}>Next</Button>
-            </View>
-        </View>
-      </NativeBaseProvider>
-      );
-  }
-  return (<View/>)
+              }}>
+              <MaterialIcons name="camera-alt" size={60} color="#FFFFFF" /></TouchableHighlight>
+              :
+              <></>
+            }
 
+
+            {picTaken && modalText.length > 0 ? 
+            <TouchableHighlight activeOpacity={0.6} underlayColor={ 'transparent' } 
+            onPress={() => {setPicTaken(false); setPicURI(""); setModalText("")}}>
+                <MaterialIcons name="cancel" size={60} color="#EA4335" /> 
+            </TouchableHighlight>
+            : <></>}  
+
+            {picTaken && modalText.length > 0 ? 
+                  <Button style={style.nextButton} onPress={() => {setModalVisible(true)}}>Next</Button>
+              
+             : <></>}  
+
+            
+          </View>
+          
+        </View>
+
+      </NativeBaseProvider>
+    );
   }
+  return (<View />)
+
+}
+
