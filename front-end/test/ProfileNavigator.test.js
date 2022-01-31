ca2669585f931f0d7f7f39a5ad6e684d59e3eca2
pirasunt <piravien.sunt@hotmail.com>
2022-01-30 21:26:58 -0500

    test: fix profilenavigator basic test #187

--- front-end/test/ProfileNavigator.test.js
+++ front-end/test/ProfileNavigator.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import {ProfileNavigator} from '../components/profile/components/ProfileNavigator'
+import {ProfileNavigator} from '../components/profile/ProfileNavigator'
+import { NavigationContainer } from "@react-navigation/native";
 
 it('renders correctly', async () => {
-    const tree = await renderer.create(<ProfileNavigator/>).toJSON();
+    const tree = await renderer.create(
+    <NavigationContainer>
+      <ProfileNavigator/>
+       </NavigationContainer>).toJSON();
     expect(tree).toMatchSnapshot();
   });
