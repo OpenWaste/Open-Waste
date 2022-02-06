c1c984fd27ee8e2a3a03f1331a04731f8fef0539
Pasha Pishdad <pashapishdad@Pashas-MacBook-Pro.local>
2022-02-06 06:18:30 -0500

    added unit test for submit button

--- front-end/test/ImageSubmission.test.js
+++ front-end/test/ImageSubmission.test.js
@@ -1,8 +1,31 @@
+import { Button, NativeBaseProvider } from 'native-base';
 import React from 'react';
+import { FlatList } from 'react-native-gesture-handler';
 import renderer from 'react-test-renderer';
-import {ForgotPassword} from '../components/profile/components/ForgotPassword'
+import {ImageSubmission} from '../components/camera/ImageSubmission/ImageSubmission'
+jest.useFakeTimers()
 
 it('renders correctly', async () => {
-    const tree = await renderer.create(<ForgotPassword/>).toJSON();
+    const tree = await renderer.create(<ImageSubmission/>).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+it('should find the button via testId', () => {
+  // const testIdName = 'pressMeButton';
+
+  // const {getByTestId} = render(<ImageSubmission />);
+
+  // const submitButton = getByTestId(testIdName);
+
+  // expect(submitButton).toBeTruthy();
+
+  const tree = renderer.create(
+    <NativeBaseProvider>
+      <FlatList>
+
+      </FlatList>
+    </NativeBaseProvider>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
