0541c0f3d6b2f69f231c1f966da72f5c2ff3cee2
Pasha Pishdad <pashapishdad@Pashas-MacBook-Pro.local>
2022-02-06 17:18:50 -0500

    added unit tests

--- front-end/test/ImageSubmission.test.js
+++ front-end/test/ImageSubmission.test.js
@@ -1,6 +1,5 @@
 import { Button, NativeBaseProvider } from 'native-base';
 import React from 'react';
-import { FlatList } from 'react-native-gesture-handler';
 import renderer from 'react-test-renderer';
 import {ImageSubmission} from '../components/camera/ImageSubmission/ImageSubmission'
 jest.useFakeTimers()
@@ -10,22 +9,36 @@ it('renders correctly', async () => {
     expect(tree).toMatchSnapshot();
   });
 
-it('should find the button via testId', () => {
-  // const testIdName = 'pressMeButton';
-
-  // const {getByTestId} = render(<ImageSubmission />);
-
-  // const submitButton = getByTestId(testIdName);
-
-  // expect(submitButton).toBeTruthy();
-
+it('submit button renders correctly', () => {
   const tree = renderer.create(
     <NativeBaseProvider>
-      <FlatList>
-
-      </FlatList>
+      <Button>
+        
+      </Button>
     </NativeBaseProvider>
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
+it('should find the button via testId', () => {
+  const tree = renderer.create(
+    <NativeBaseProvider>
+      <View>
+        <Box m="10">
+          <VStack space={8}>
+            <Box width="100%">
+              <Heading fontWeight="medium" style={styles.header}>
+                Image Submission
+              </Heading>
+              <Text mt="3" style={styles.text}>
+                If the app was not able to detect your item, upload a picture of
+                it so that it can be used to help improve the app.
+              </Text>
+            </Box>
+          </VStack>
+        </Box>
+      </View>
+    </NativeBaseProvider>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
