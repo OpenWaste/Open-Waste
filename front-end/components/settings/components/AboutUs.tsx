404134f87e10555911208299ba2fd58af3b8c100
Pasha Pishdad <pashapishdad@Pashas-MacBook-Pro.local>
2022-03-02 17:43:07 -0500

    added social media part to the about us page.

--- front-end/components/settings/components/AboutUs.tsx
+++ front-end/components/settings/components/AboutUs.tsx
@@ -3,7 +3,8 @@ import { ScrollView, View, Text, Image, Button, TouchableHighlight } from "react
 import style from "./styles/AboutUs";
 import { NativeBaseProvider } from "native-base";
 import { Linking } from 'react-native';
-import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import Clipboard from '@react-native-community/clipboard';
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 
 export class AboutUs extends React.Component {
@@ -60,6 +61,71 @@ We are a group of students across all four faculties united by the goal of prote
               <View style={{ flex: 2, marginTop: 5 }} >
                 <Button onPress={() => Linking.openURL('mailto:zerowaste@concordia.ca') }
         title="zerowaste@concordia.ca" />
+              </View>
+            </View>
+          </View>
+
+          <View style={{marginTop: 10, marginBottom: 10, marginLeft: 20, marginRight: 20, borderRadius: 5, backgroundColor: 'white'}}>
+            <Text style={style.textHeader}>Social Media </Text>
+            <View style={[{
+              flexDirection: "row"
+            }]}>
+              <View style={{ flex: 2, marginTop: 5}} >
+                <Button onPress={() => Clipboard.setString('cp3@concordia.ca')}
+        title="cp3@concordia.ca" />
+              </View>
+              <View style={{ flex: 2, marginTop: 5 }} >
+                <Button onPress={() => Clipboard.setString('zerowaste@concordia.ca')}
+        title="zerowaste@concordia.ca" />
+              </View>
+            </View>
+            
+            <View style={[{
+              flexDirection: "row"
+            }]}>
+              <View style={[{
+                flexDirection: "row"
+              }]}>
+                <View>
+                  <TouchableHighlight onPress={() => Linking.openURL('https://www.facebook.com/CP3Concordia/')}>
+                  <Image source={require("../../../assets/facebook.png")} style = {{width: 25, height: 25, flex: 1, marginLeft: 20}}>
+                  </Image>
+                  </TouchableHighlight>
+                </View>
+                <View>
+                  <TouchableHighlight onPress={() => Linking.openURL('https://www.instagram.com/cp3concordia/?hl=en')}>
+                  <Image source={require("../../../assets/instagram.png")} style = {{width: 25, height: 25, flex: 1, marginLeft: 20}}>
+                  </Image>
+                  </TouchableHighlight>
+                </View>
+                <View>
+                  <TouchableHighlight onPress={() => Linking.openURL('https://www.cp3montreal.com/')}>
+                  <Image source={require("../../../assets/web.png")} style = {{width: 25, height: 25, flex: 1, marginLeft: 20}}>
+                  </Image>
+                  </TouchableHighlight>
+                </View>
+              </View>
+              <View style={[{
+                flexDirection: "row", marginLeft: 40, marginBottom: 10
+              }]}>
+                <View>
+                  <TouchableHighlight onPress={() => Linking.openURL('https://www.facebook.com/zerowasteconcordia/')}>
+                  <Image source={require("../../../assets/facebook.png")} style = {{width: 25, height: 25, flex: 1, marginLeft: 20}}>
+                  </Image>
+                  </TouchableHighlight>
+                </View>
+                <View>
+                  <TouchableHighlight onPress={() => Linking.openURL('https://www.instagram.com/zerowasteconcordia/?hl=en')}>
+                  <Image source={require("../../../assets/instagram.png")} style = {{width: 25, height: 25, flex: 1, marginLeft: 20}}>
+                  </Image>
+                  </TouchableHighlight>
+                </View>
+                <View>
+                  <TouchableHighlight onPress={() => Linking.openURL('https://www.concordia.ca/about/sustainability/sustainability-initiatives/zero-waste.html?utm_source=vanity&utm_campaign=zerowaste')}>
+                  <Image source={require("../../../assets/web.png")} style = {{width: 25, height: 25, flex: 1, marginLeft: 20}}>
+                  </Image>
+                  </TouchableHighlight>
+                </View>
               </View>
             </View>
           </View>
