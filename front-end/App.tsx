8ef1e88c5148e9eabd59d996d0cd855036008431
pirasunt <piravien.sunt@hotmail.com>
2022-03-12 14:23:22 -0500

    task: ensured that location permission are requested #18

--- front-end/App.tsx
+++ front-end/App.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import { MainContainer } from "./components/MainContainer";
 import Service from "./service/service";
 import "react-native-gesture-handler";
+import * as Location from 'expo-location';
 
 export default function App() {
   
   React.useEffect(() => {
     Service.updateApplicationCache()
+    Location.requestForegroundPermissionsAsync().then(() => {})
   });
 
   return <MainContainer />;
