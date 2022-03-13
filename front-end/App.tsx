158fece51d8723b131a890cfc8b331ed95061a06
CeliaCaii <47066791+CeliaCaii@users.noreply.github.com>
2022-03-13 16:11:18 -0400

    task: add the 1st iteration of onboarding screens #261

--- front-end/App.tsx
+++ front-end/App.tsx
@@ -1,13 +1,27 @@
 import * as React from "react";
 import { MainContainer } from "./components/MainContainer";
+import OnboardingScreen from "./components/OnboardingScreen";
 import Service from "./service/service";
-import "react-native-gesture-handler";
 
-export default function App() {
-  
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const AppStack = createStackNavigator();
+
+const App = () => {
   React.useEffect(() => {
     Service.updateApplicationCache()
   });
-
-  return <MainContainer />;
+  
+  return (
+    <NavigationContainer independent={true}>
+      <AppStack.Navigator screenOptions={{headerMode: "false"}}>
+        <AppStack.Screen name = "OnboardingScreen" component={OnboardingScreen} />
+        <AppStack.Screen name = "MainContainer" component={MainContainer} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
