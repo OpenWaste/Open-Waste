788ed18e684aee59ac34115814274e76e174e397
ngjean <jean05997@gmail.com>
2022-03-17 17:05:27 -0400

    chore: reorganize some function and fix formatting #293
    noted by @SiuYe

--- front-end/components/profile/components/EditProfile.tsx
+++ front-end/components/profile/components/EditProfile.tsx
@@ -1,29 +1,47 @@
 import React, { useEffect } from "react";
-import { View, ScrollView, KeyboardAvoidingView, Text, Image } from "react-native";
-import { Accordion, AlertDialog, Box, Button, Center, NativeBaseProvider, Input } from 'native-base';
-import { save, getValueFor, deleteValueFor } from '../../../utils/PersistInfo';
+import {
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Text,
+  Image,
+} from "react-native";
+import {
+  Accordion,
+  AlertDialog,
+  Box,
+  Button,
+  Center,
+  NativeBaseProvider,
+  Input,
+} from "native-base";
+import { save, getValueFor, deleteValueFor } from "../../../utils/PersistInfo";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import Service from "../../../service/service";
 import { UserResource } from "../../../models/User";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 import { showMessage } from "react-native-flash-message";
 
 import style from "./styles/edit-profile";
 import formStyle from "./styles/forms";
-import isEmail from 'validator/lib/isEmail';
+import isEmail from "validator/lib/isEmail";
 
 export class EditProfile extends React.Component {
-  
   render() {
     return (
       <NativeBaseProvider>
         <ScrollView>
           <KeyboardAvoidingView>
-              <View style={style.header}></View> 
-              {/* TO DO: Pull profile pic from database. */}
-              <Image style={style.profilePic} source={{uri: 'https://www.gravatar.com/avatar/d41d8cd98f00b204e9800998ecf8427e?size=192&d=mm'}} />
-              <Text style={style.username}> Edit Profile </Text>
-              <EditForm />
+            <View style={style.header}></View>
+            {/* TO DO: Pull profile pic from database. */}
+            <Image
+              style={style.profilePic}
+              source={{
+                uri: "https://www.gravatar.com/avatar/d41d8cd98f00b204e9800998ecf8427e?size=192&d=mm",
+              }}
+            />
+            <Text style={style.username}> Edit Profile </Text>
+            <EditForm />
           </KeyboardAvoidingView>
         </ScrollView>
       </NativeBaseProvider>
@@ -32,80 +50,102 @@ export class EditProfile extends React.Component {
 }
 
 export function EditForm() {
+  const navigation = useNavigation();
+  const [oldUsername, setOldUsername] = React.useState("");
+  const [newUsername, setNewUsername] = React.useState("");
+  const [oldEmail, setOldEmail] = React.useState("");
+  const [newEmail, setNewEmail] = React.useState("");
+  const [isChange, setIsChange] = React.useState(false);
 
-  const navigation=useNavigation();
-  const [oldUsername, setOldUsername]=React.useState('');
-  const [newUsername, setNewUsername]=React.useState('');
-  const [oldEmail, setOldEmail]=React.useState('');
-  const [newEmail, setNewEmail]=React.useState('');
-  const [isChange, setIsChange]=React.useState(false);
-
-  useEffect(()=>{
-    getValueFor('username').then(output => {
+  useEffect(() => {
+    getValueFor("username").then((output) => {
       setOldUsername(output);
       setNewUsername(output);
     });
 
-    getValueFor('email').then(output => {
+    getValueFor("email").then((output) => {
       setOldEmail(output);
       setNewEmail(output);
     });
-  }, [])
+  }, []);
 
   const handleCancel = () => {
-    navigation.navigate('ProfilePage');
-  }
+    navigation.navigate("ProfilePage");
+  };
 
-  const handleSubmit=() => {
+  const handleSubmit = () => {
+    if (isChange) {
+      if (newUsername == "") {
+        showMessage({ message: "Please fill a new username", type: "warning" });
+        return;
+      } else if (newEmail == "") {
+        showMessage({ message: "Please fill a new email", type: "warning" });
+        return;
+      } else if (!isEmail(newEmail)) {
+        showMessage({ message: "Invalid Email", type: "warning" });
+        return;
+      }
 
-    if(isChange)
-    {
-      if(newUsername==''){
-        showMessage({ message: "Please fill a new username", type: "warning" })
-        return;
-      }
-      else if(newEmail==''){
-        showMessage({ message: "Please fill a new email", type: "warning" })
-        return;
-      }
-      else if(!isEmail(newEmail)){
-        showMessage({ message: 'Invalid Email', type: 'warning' });
-        return;
-      }
-        
-      const user={
+      const user = {
         old_username: oldUsername,
         new_username: newUsername,
         email: newEmail,
-      }
+      };
 
       // Get response from update-username-email endpoint
-      Service.updateUsernameEmail(user).then(() => {
-        save('username', newUsername);
-        save('email', newEmail);
+      Service.updateUsernameEmail(user)
+        .then(() => {
+          save("username", newUsername);
+          save("email", newEmail);
 
-        navigation.navigate('ProfilePage');
-        showMessage({ message: 'Successfully Updated Account', type: 'success' });
-      }).catch(() =>{
-        showMessage({ message: 'An Error Has Occurred', type: 'warning' });
-      });
+          navigation.navigate("ProfilePage");
+          showMessage({
+            message: "Successfully Updated Account",
+            type: "success",
+          });
+        })
+        .catch(() => {
+          showMessage({ message: "An Error Has Occurred", type: "warning" });
+        });
+    } else {
+      navigation.navigate("ProfilePage");
     }
-    else
-    {
-      navigation.navigate('ProfilePage');
-    }
-  }
+  };
 
-  return(
+  const setValue = (value: any, category: string) => {
+    if (category == "email") {
+      setNewEmail(value);
+    } else if (category == "user") {
+      setNewUsername(value);
+    }
+
+    setIsChange(true);
+    return;
+  };
+
+  return (
     <View>
       <Box m={3}>
         <Accordion>
           <Accordion.Item testID="editUsernameAccordion">
-            <Accordion.Summary _expanded={{ backgroundColor: '#0F968D' }}>Edit Username<Accordion.Icon /></Accordion.Summary>
+            <Accordion.Summary _expanded={{ backgroundColor: "#0F968D" }}>
+              Edit Username
+              <Accordion.Icon />
+            </Accordion.Summary>
             <Accordion.Details>
               <View style={formStyle.accordionInputView}>
-                <MaterialIcons style={formStyle.registrationIcons} name="person" size={22}/>
-                <Input style={formStyle.registrationTextInputs} onChangeText={(value:any) => {setNewUsername(value); setIsChange(true)}} borderColor="transparent">
+                <MaterialIcons
+                  style={formStyle.registrationIcons}
+                  name="person"
+                  size={22}
+                />
+                <Input
+                  style={formStyle.registrationTextInputs}
+                  onChangeText={(value: any) => {
+                    setValue(value, "user");
+                  }}
+                  borderColor="transparent"
+                >
                   {oldUsername}
                 </Input>
               </View>
@@ -117,11 +157,24 @@ export function EditForm() {
       <Box m={3}>
         <Accordion>
           <Accordion.Item testID="editEmailAccordion">
-            <Accordion.Summary _expanded={{ backgroundColor: '#0F968D' }}> Edit Email <Accordion.Icon/></Accordion.Summary>
+            <Accordion.Summary _expanded={{ backgroundColor: "#0F968D" }}>
+              {" "}
+              Edit Email <Accordion.Icon />
+            </Accordion.Summary>
             <Accordion.Details>
               <View style={formStyle.accordionInputView}>
-                <MaterialIcons style={formStyle.registrationIcons} name="alternate-email" size={22}/>
-                <Input style={formStyle.registrationTextInputs} onChangeText={(value:any) => {setNewEmail(value); setIsChange(true)}} borderColor="transparent">
+                <MaterialIcons
+                  style={formStyle.registrationIcons}
+                  name="alternate-email"
+                  size={22}
+                />
+                <Input
+                  style={formStyle.registrationTextInputs}
+                  onChangeText={(value: any) => {
+                    setValue(value, "email");
+                  }}
+                  borderColor="transparent"
+                >
                   {oldEmail}
                 </Input>
               </View>
@@ -129,82 +182,104 @@ export function EditForm() {
           </Accordion.Item>
         </Accordion>
       </Box>
-      <DeleteAccount/>
+      <DeleteAccount />
       <View style={style.btnView}>
         <Button
-          testID="cancelBtn" 
-          style={style.cancelBtn} 
-          onPress={handleCancel}> Cancel </Button>
-        <Button 
-          testID="saveBtn"
-          style={style.saveBtn} 
-          onPress={handleSubmit}> Save </Button>
-      </View> 
+          testID="cancelBtn"
+          style={style.cancelBtn}
+          onPress={handleCancel}
+        >
+          {" "}
+          Cancel{" "}
+        </Button>
+        <Button testID="saveBtn" style={style.saveBtn} onPress={handleSubmit}>
+          {" "}
+          Save{" "}
+        </Button>
+      </View>
     </View>
-  )
+  );
 }
 
 export function DeleteAccount() {
-  const [isOpen, setIsOpen]=React.useState(false);
-  const [username, setUsername]=React.useState('');
-  const onClose=() => setIsOpen(false)
-  const cancelRef=React.useRef(null);
-  const navigation=useNavigation();
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [username, setUsername] = React.useState("");
+  const onClose = () => setIsOpen(false);
+  const cancelRef = React.useRef(null);
+  const navigation = useNavigation();
 
-  getValueFor('username').then(output => {
+  getValueFor("username").then((output) => {
     setUsername(output);
-  }) 
+  });
 
-  const handleDelete=() => {
-    
-    const user: UserResource={
+  const handleDelete = () => {
+    const user: UserResource = {
       username: username,
-      email: '',
-      password: '',
-    }
+      email: "",
+      password: "",
+    };
 
-    Service.deleteUser(user).then(() => {
-      // If response is good, delete persistent data
-      deleteValueFor('username');
-      deleteValueFor('email');
-      deleteValueFor('submitted_images');
-      deleteValueFor('accepted_images');
-      // Redirect and show success message
-      navigation.navigate('ProfilePage');
-      showMessage({ message: 'Successfully Deleted Account', type: 'success' })
-    }).catch(() => {
-      showMessage({ message: 'An Error Has Occurred', type: 'warning' });
-    })
+    Service.deleteUser(user)
+      .then(() => {
+        // If response is good, delete persistent data
+        deleteValueFor("username");
+        deleteValueFor("email");
+        deleteValueFor("submitted_images");
+        deleteValueFor("accepted_images");
+        // Redirect and show success message
+        navigation.navigate("ProfilePage");
+        showMessage({
+          message: "Successfully Deleted Account",
+          type: "success",
+        });
+      })
+      .catch(() => {
+        showMessage({ message: "An Error Has Occurred", type: "warning" });
+      });
 
     onClose();
-  }
+  };
 
   return (
     <Center>
       <Button
         testID="deleteBtn"
         style={style.deleteBtn}
-        _text={{color:'#D33333', paddingTop: 2, paddingBottom: 2}}
+        _text={{ color: "#D33333", paddingTop: 2, paddingBottom: 2 }}
         colorScheme="danger"
         onPress={() => setIsOpen(!isOpen)}
       >
         Delete account
       </Button>
-      <AlertDialog leastDestructiveRef={cancelRef} isOpen={isOpen} onClose={onClose}>
-      <AlertDialog.Content>
-      <AlertDialog.CloseButton/>
-      <AlertDialog.Header>Are you sure?</AlertDialog.Header>
-      <AlertDialog.Body>
-        This action will delete all your data from your device and cloud. This action is irreversable.
-      </AlertDialog.Body>
-      <AlertDialog.Footer>
-        <Button.Group space={2}>
-          <Button colorScheme="danger" onPress={handleDelete}>Delete</Button>
-          <Button variant="unstyled" colorScheme="coolGray" onPress={onClose} ref={cancelRef}>Cancel</Button>                   
-        </Button.Group>
-      </AlertDialog.Footer>
-      </AlertDialog.Content>
-    </AlertDialog>
+      <AlertDialog
+        leastDestructiveRef={cancelRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <AlertDialog.Content>
+          <AlertDialog.CloseButton />
+          <AlertDialog.Header>Are you sure?</AlertDialog.Header>
+          <AlertDialog.Body>
+            This action will delete all your data from your device and cloud.
+            This action is irreversable.
+          </AlertDialog.Body>
+          <AlertDialog.Footer>
+            <Button.Group space={2}>
+              <Button colorScheme="danger" onPress={handleDelete}>
+                Delete
+              </Button>
+              <Button
+                variant="unstyled"
+                colorScheme="coolGray"
+                onPress={onClose}
+                ref={cancelRef}
+              >
+                Cancel
+              </Button>
+            </Button.Group>
+          </AlertDialog.Footer>
+        </AlertDialog.Content>
+      </AlertDialog>
     </Center>
-  )
+  );
 }
