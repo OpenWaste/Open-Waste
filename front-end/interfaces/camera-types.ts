48bcca4265aef1ee2fabb4fc60fa34e05f12618d
Siu Ye <SiuTongYe@gmail.com>
2022-03-23 22:18:07 -0400

    feat: display images of bin #290

--- front-end/interfaces/camera-types.ts
+++ front-end/interfaces/camera-types.ts
@@ -1,52 +1,59 @@
-import {Bin, Building, PredictionResponse} from "./service-types";
+import { Bin, Building, PredictionResponse } from "./service-types";
 import { Camera } from "expo-camera";
 
-export interface MapModalProperties{
-    category:string,
-    currentVisibilty:boolean,
-    visibilitySetter: (value:boolean) => void
-  }
+export interface MapModalProperties {
+  category: string;
+  currentVisibilty: boolean;
+  visibilitySetter: (value: boolean) => void;
+}
 
 export interface CameraTriggerButtonProperties {
-  uriSetter: (value:any) => void,
-  isPictureTakenSetter: (value:boolean) => void,
-  predictionTextSetter: (predictionText:string) => void,
-  predictionFetcher: (base64Image:string|undefined) => Promise<PredictionResponse>,
-  camera:Camera
+  uriSetter: (value: any) => void;
+  isPictureTakenSetter: (value: boolean) => void;
+  predictionTextSetter: (predictionText: string) => void;
+  predictionFetcher: (
+    base64Image: string | undefined
+  ) => Promise<PredictionResponse>;
+  camera: Camera;
 }
 
 export interface PredictionTextProperties {
-  predictionString: string
+  predictionString: string;
 }
 
 export interface PostPictureSnapButtonsProperties {
-  uriSetter: (value:any) => void,
-  isPictureTakenSetter: (value:boolean) => void,
-  predictionTextSetter: (predictionText:string) => void,
-  visibilitySetter: (value:boolean) => void
+  uriSetter: (value: any) => void;
+  isPictureTakenSetter: (value: boolean) => void;
+  predictionTextSetter: (predictionText: string) => void;
+  visibilitySetter: (value: boolean) => void;
 }
 
 export interface MapBottomSheetProperties {
-  category:string
-  instruction:string|undefined
-  closestBuilding:Building|undefined
-  bins:Bin[]|undefined
+  category: string;
+  instruction: string | undefined;
+  closestBuilding: Building | undefined;
+  bins: Bin[] | undefined;
 }
 
 export interface PicturePreviewProperties {
-  pictureURI: string
+  pictureURI: string;
+}
+
+export interface BinWithImage {
+  bin: Bin;
+  imageBase64: string;
 }
 
 export interface CameraViewProperties {
-  isPictureTaken:boolean,
-  predictionString: string,
-  modalVisibility:boolean,
-  navigator:any
-  cameraInstance:Camera|null
-  pictureURI: string,
-  cameraInstanceSetter: (instance:Camera) => void,
-  modalVisibilitySetter: (value:boolean) => void,
-  isPictureTakenSetter: (value:boolean) => void,
-  predictionTextSetter: (predictionText:string) => void,
-  uriSetter: (value:any) => void,
+  isPictureTaken: boolean;
+  predictionString: string;
+  modalVisibility: boolean;
+  navigator: any;
+  cameraInstance: Camera | null;
+  pictureURI: string;
+  cameraInstanceSetter: (instance: Camera) => void;
+  modalVisibilitySetter: (value: boolean) => void;
+  isPictureTakenSetter: (value: boolean) => void;
+  predictionTextSetter: (predictionText: string) => void;
+  uriSetter: (value: any) => void;
 }
