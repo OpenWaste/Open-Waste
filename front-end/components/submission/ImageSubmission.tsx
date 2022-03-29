4cbf6021d350821531dbe72b5194592fde4b1899
ngjean <jean05997@gmail.com>
2022-03-29 19:03:42 -0400

    test: add more test to reach 80% coverage #310

--- front-end/components/submission/ImageSubmission.tsx
+++ front-end/components/submission/ImageSubmission.tsx
@@ -36,47 +36,6 @@ export function ImageSubmission() {
   const [isError, setIsError] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(false);
 
-  const onClose = () => {
-    setIsOpen(false),setIsError(false),setIsLoading(false);
-  };
-
-  //Opens the camera roll
-  const pickImage = async () => {
-    let res = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: false,
-    });
-
-    if (!res.cancelled) {
-      setImage(res);
-      setImageIsChosen(!imageIsChosen);
-      setImageIsChosen(true);
-    }
-  };
-
-  //Calls the service to submit using POST
-  const handleSubmit = async () => {
-    setIsLoading(true);
-    try {
-      let convertedImage = await manipulateAsync(image.uri,[],{ compress: 1, format: SaveFormat.JPEG, base64: true });
-      await Service.submitImageCategory(convertedImage.base64, category)
-
-      setIsOpen(!isOpen);
-      setImageIsChosen(false);
-      setCategory("");
-
-      
-      let email = await getValueFor("email").catch(()=>{})
-      if(email != undefined) {
-        let numberOfSubmittedImages = await getValueFor("submitted_images").catch(() => save("submitted_images", 1))
-        save("submitted_images", +numberOfSubmittedImages + 1);
-      }
-    }
-    catch {
-      setIsError(true);
-    }
-  };
-
   //Gets list of categories from endpoint
   useEffect(() => {
     if (categoriesList.length == 0) {
@@ -101,10 +60,9 @@ export function ImageSubmission() {
         setIsOpen={setIsOpen}
         isError={isError}
         setIsError={setIsError}
-        onClose={onClose}
-        pickImage={pickImage}
-        handleSubmit={handleSubmit}
         isLoading={isLoading}
+        setIsLoading={setIsLoading}
+        submitImageCategory={submitImageCategory}
       />
     </NativeBaseProvider>
   );
@@ -126,10 +84,20 @@ export const ImageSubmissionView = (prop) => {
             </Text>
           </Box>
           {prop.imageIsChosen ? (
-            <Button variant="unstyled" onPress={prop.pickImage}>
+            <Button
+              testID="ImageIconButton"
+              variant="unstyled"
+              onPress={() =>
+                pickImage(
+                  prop.setImage,
+                  prop.setImageIsChosen,
+                  prop.imageIsChosen
+                )
+              }
+            >
               <AspectRatio w="100%" ratio={1}>
                 <Image
-                  testID = "ImageButton"
+                  testID="ImageButton"
                   style={{ resizeMode: "contain" }}
                   width="100%"
                   height="100%"
@@ -148,20 +116,26 @@ export const ImageSubmissionView = (prop) => {
               style={styles.box}
             >
               <Button
-                testID = "ImageIconButton"
-                onPress={prop.pickImage}
+                testID="IconButton"
+                onPress={() =>
+                  pickImage(
+                    prop.setImage,
+                    prop.setImageIsChosen,
+                    prop.imageIsChosen
+                  )
+                }
                 variant="unstyled"
                 width="100%"
                 height="100%"
               >
                 <MaterialCommunityIcons
-                   name="image-outline"
+                  name="image-outline"
                   size={50}
-                   color="#8A8A8A"
-                 />
+                  color="#8A8A8A"
+                />
               </Button>
             </Box>
-            )}        
+          )}
           <Box>
             <FormControl isRequired>
               <FormControl.Label mt="4" color="#8A8A8A">
@@ -181,14 +155,34 @@ export const ImageSubmissionView = (prop) => {
                   );
                 })}
               </Select>
-              <Box m="10">               
-                <Text style={styles.submitBttn}
-                onPress={prop.handleSubmit}> {i18next.t('Submit')} </Text>
+              <Box m="10">
+                <Button
+                  testID="SubmitButton"
+                  isLoading={prop.isLoading}
+                  isLoadingText="Submitting"
+                  _loading={{
+                    bg: "primary.500",
+                  }}
+                  onPress={() =>
+                    handleSubmit(
+                      prop.setIsLoading,
+                      prop.setIsOpen,
+                      prop.isOpen,
+                      prop.setIsError,
+                      prop.setImageIsChosen,
+                      prop.setCategory,
+                      prop.image,
+                      prop.category,
+                      prop.submitImageCategory
+                    )
+                  }
+                >
+                  {i18next.t('Submit')}
+                </Button>
                 <Center>
                   <AlertDialog
                     leastDestructiveRef={cancelRef}
                     isOpen={prop.isOpen}
-                    onClose={prop.onClose}
                   >
                     <AlertDialog.Content>
                       <AlertDialog.Header> {i18next.t('Success')} </AlertDialog.Header>
@@ -197,8 +191,15 @@ export const ImageSubmissionView = (prop) => {
                       </AlertDialog.Body>
                       <AlertDialog.Footer>
                         <Button.Group space={2}>
-                          <Button testID = "SuccessAlert"
-                            onPress={prop.onClose}
+                          <Button
+                            testID="SuccessAlert"
+                            onPress={() =>
+                              onClose(
+                                prop.setIsOpen,
+                                prop.setIsError,
+                                prop.setIsLoading
+                              )
+                            }
                             ref={cancelRef}
                           >
                             {i18next.t('Cancel')}
@@ -215,7 +216,6 @@ export const ImageSubmissionView = (prop) => {
                   <AlertDialog
                     leastDestructiveRef={cancelRef}
                     isOpen={prop.isError}
-                    onClose={prop.onClose}
                   >
                     <AlertDialog.Content>
                       <AlertDialog.CloseButton />
@@ -225,9 +225,16 @@ export const ImageSubmissionView = (prop) => {
                       </AlertDialog.Body>
                       <AlertDialog.Footer>
                         <Button.Group space={2}>
-                          <Button testID = "ErrorAlert"
+                          <Button
+                            testID="ErrorAlert"
                             style={styles.cancelBttn}
-                            onPress={prop.onClose}
+                            onPress={() =>
+                              onClose(
+                                prop.setIsOpen,
+                                prop.setIsError,
+                                prop.setIsLoading
+                              )
+                            }
                             ref={prop.cancelRef}
                           >
                             {i18next.t('Cancel')}
@@ -247,4 +254,70 @@ export const ImageSubmissionView = (prop) => {
       </Box>
     </View>
   );
+};
+
+//Calls the service to submit using POST
+export const handleSubmit = async (
+  setIsLoading,
+  setIsOpen,
+  isOpen,
+  setIsError,
+  setImageIsChosen,
+  setCategory,
+  image,
+  category,
+  submitImageCategory
+) => {
+  setIsLoading(true);
+  try {
+    await submitImageCategory(image, category);
+    setIsOpen(!isOpen);
+    setImageIsChosen(false);
+    setCategory("");
+    updateSubmittedImage();
+  } catch {
+    setIsError(true);
+  }
+};
+
+export const submitImageCategory = async (image, category) => {
+  let convertedImage = await manipulateAsync(image.uri, [], {
+    compress: 1,
+    format: SaveFormat.JPEG,
+    base64: true,
+  });
+  let response = await Service.submitImageCategory(
+    convertedImage.base64,
+    category
+  );
+
+  if (response == undefined) throw new Error("Error");
+};
+
+const updateSubmittedImage = async () => {
+  let email = await getValueFor("email").catch(() => {});
+  if (email != undefined) {
+    let numberOfSubmittedImages = await getValueFor("submitted_images").catch(
+      () => save("submitted_images", 1)
+    );
+    save("submitted_images", +numberOfSubmittedImages + 1);
+  }
+};
+
+//Opens the camera roll
+export const pickImage = async (setImage, setImageIsChosen, imageIsChosen) => {
+  let res = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: false,
+  });
+
+  if (!res.cancelled) {
+    setImage(res);
+    setImageIsChosen(!imageIsChosen);
+    setImageIsChosen(true);
+  }
+};
+
+export const onClose = (setIsOpen, setIsError, setIsLoading) => {
+  setIsOpen(false), setIsError(false), setIsLoading(false);
 };
